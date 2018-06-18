c5d6ceb4031d554eb7c3f16cd810ebc36f2a6efe
Gabriele Oliaro <ict@gabrieleoliaro.it>
2018-06-18 12:00:16 +0200

    finished covSEiso

--- covSEiso.py
+++ covSEiso.py
@@ -3,6 +3,8 @@
 import numpy as np
 import math
 
+from sq_dist import *
+
 def covSEisoCM(loghyper, x, z):
     """
     Squared Exponential covariance function with isotropic distance measure. The 
@@ -26,4 +28,63 @@ def covSEisoCM(loghyper, x, z):
     ell = np.exp(loghyper[0])                 # Characteristic length scale
     sf2 = np.exp(2 * loghyper[1])             # Signal variance
 
-    A = sf2 * np.exp(-sq_dist(x.
+    # Compute Covariance Matrix
+    A = sf2 * np.exp(-sq_distONE(x.conj().transpose() / ell) / 2)
+
+def covSEisoTST(loghyper, x, z):
+    """
+    Squared Exponential covariance function with isotropic distance measure. The 
+    covariance function is parameterized as:
+    
+    k(x^p,x^q) = sf2 * exp(-(x^p - x^q)'*inv(P)*(x^p - x^q)/2) 
+    
+    where the P matrix is ell^2 times the unit matrix and sf2 is the signal
+    variance. The hyperparameters are:
+    
+    loghyper = [ log(ell)
+                 log(sqrt(sf2)) ]
+
+    TSC stands for test set covariances
+    
+    For more help on design of covariance functions, try "help covFunctions".
+    
+    (C) Copyright 2006 by Carl Edward Rasmussen (2007-06-25)
+    """
+    [n D] = x.shape
+    ell = np.exp(loghyper[0])                 # Characteristic length scale
+    sf2 = np.exp(2 * loghyper[1])             # Signal variance
+
+    # Compute Test Set Covariances
+    A = sf2 * np.ones(z.shape[0], 1)
+    B = sf2 * np.exp(-sq_distTWO(x.conj().transpose() / ell, z.conj().transpose() / ell) / 2)
+    
+
+def covSEisoDERIV(loghyper, x, z):
+    """
+    Squared Exponential covariance function with isotropic distance measure. The 
+    covariance function is parameterized as:
+    
+    k(x^p,x^q) = sf2 * exp(-(x^p - x^q)'*inv(P)*(x^p - x^q)/2) 
+    
+    where the P matrix is ell^2 times the unit matrix and sf2 is the signal
+    variance. The hyperparameters are:
+    
+    loghyper = [ log(ell)
+                 log(sqrt(sf2)) ]
+
+    TSC stands for test set covariances
+    
+    For more help on design of covariance functions, try "help covFunctions".
+    
+    (C) Copyright 2006 by Carl Edward Rasmussen (2007-06-25)
+    """
+    [n D] = x.shape
+    ell = np.exp(loghyper[0])                 # Characteristic length scale
+    sf2 = np.exp(2 * loghyper[1])             # Signal variance
+
+    # Compute Derivative Matrix
+    if z == 1:                              # First parameter
+        A = sf2 * np.multiply(np.exp(-sq_distONE(x.conj().transpose() / ell) / 2), sq_distONE(x.conj().transpose() / ell))
+    else:                                   # Second parameter
+        A = 2 * sf2 * np.exp(-sq_distONE(x.conj().transpose() / ell) / 2)
+        
