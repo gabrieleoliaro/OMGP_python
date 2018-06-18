c708af6696650fb35310df6140ddd6d1b159e77f
Gabriele Oliaro <ict@gabrieleoliaro.it>
2018-06-18 11:33:30 +0200

    finished sq_dist.py

--- quality.py
+++ quality.py
@@ -5,12 +5,18 @@ import numpy as np
 def quality(Y, mu, C, pi0):
     [Ntst, D, M] = mu.shape
 
+    # Make sure all input variables are numpy matrix objects
+    Y = np.matrix(Y)
+    mu = np.matrix(mu)
+    C = np.matrix(C)
+    pi0 = np.matrix(pi0)
+
     center = np.zeros((Ntst, D))
 
     for m in range(M):
         center = center + np.matmul(pi0[m], mu[:,:,m])
 
-    NMSE = np.divide(np.mean((Y - center) ** 2), np.mean((Y - np.ones(Ntst,1) * np.mean(Y)) ** 2))
+    NMSE = np.divide(np.mean((Y - center) ** 2), np.mean((Y - np.ones(Ntst,1) * np.mean(Y, axis=0), axis=0) ** 2, axis=0), axis=0])
 
     p = 0
     for m in range(M):
