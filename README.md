2392d7d3d1217a67b172f81391b533a8dec27165
Gabriele Oliaro <mail@gabrieleoliaro.it>
2018-07-10 11:38:38 +0200

    first edits on new master branch

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # OMGP_python
-This is a translation of the Code for "Overlapping Mixtures of Gaussian Processes for the data association problem" by Miguel Lázaro-Gredilla into Python
+This is the code for a 2D Gaussian Process Regression based upon the paper "Overlapping Mixtures of Gaussian Processes for the data association problem" by Miguel Lázaro-Gredilla et al.
 
-Once we finish the translation of each function to Python, it would be nice to congregate all of them together into one or a few classes. So far each function is in its own file, and that can be probably optimized.
+The code is not very fast, so it needs to be optimized.
+Something to do is to make the program available on python versions earlier than 3.x, so that it can be run with the simple 'python' command
+It would also help to rewrite/refactor the code into OOP
 
 This program requires Python 3. If you run it on Python 3, and it still doesn't work, make sure that you have installed the required dependecies (e.g. matplotlib) not just for python, but for python3 specifically.
 To do so, you need to have pip for python 3 installed, and you have to run commands such as 'pip3 install matplotlib'.
--- test_omgp.py
+++ test_omgp.py
@@ -19,8 +19,8 @@ def test_omgp():
 
     # Number of time instants per GP, dimensions and GPs
 
-    n = 100
-    D = 3
+    n = 50
+    D = 2
     M = 4
 
 
@@ -45,13 +45,11 @@ def test_omgp():
 
     # Initialize Plot
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x_train, Y_train[:, 0], Y_train[:, 1], c='k', marker='x')  # Add scattered points
+    ax = fig.add_subplot(111)
+    ax.scatter(np.squeeze(np.asarray(x_train)), Y_train[:, 0], c='k', marker='x')  # Add scattered points
     ax.set_title("%d trajectories to be separated" % M)  # Add title
     ax.set_xlabel('X Axis')
     ax.set_ylabel('Y Axis')
-    ax.set_zlabel('Z Axis')
-    ax.view_init(elev=20, azim=30) #Works!
     plt.show()
 
     # OMGP tracking and plotting
@@ -62,26 +60,22 @@ def test_omgp():
 
     [nada, label] = qZ.max(1), qZ.argmax(1)
     fig2 = plt.figure()
-    ax2 = fig2.add_subplot(111, projection='3d')
+    ax2 = fig2.add_subplot(111)
     ax2.set_xlabel('X Axis')
     ax2.set_ylabel('Y Axis')
-    ax2.set_zlabel('Z Axis')
     plt.style.use('default')
-    ax2.view_init(elev=20, azim=30) #Works!
     ax2.set_title("OMGP regression")
     for c in range(M):
         x_train_new = np.array([])
         Y_train0_new = np.array([])
-        Y_train1_new = np.array([])
         for i in range(x_train.shape[0]):
             if (label[i] == c):
                 x_train_new = np.append(x_train_new, x_train[i])
                 Y_train0_new = np.append(Y_train0_new, Y_train[i, 0])
-                Y_train1_new = np.append(Y_train1_new, Y_train[i, 1])
-        ax2.scatter(x_train_new, Y_train0_new, Y_train1_new, c='C%i'%c, marker='x')  # Add scattered points
+        ax2.scatter(x_train_new, Y_train0_new, c='C%i'%c, marker='x')  # Add scattered points
         
 
-        ax2.plot3D(np.squeeze(np.asarray(x_test)), mu[:, 0, c], mu[:, 1, c], c='C%i'%c)
+        ax2.plot(np.squeeze(np.asarray(x_test)), mu[:, 0, c], c='C%i'%c)
     plt.show()
 
     
