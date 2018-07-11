d967bbfee93b811cffbb80eebdc65b34d475b059
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2018-07-11 17:19:13 +0200

    edited parser

--- src/parser.py
+++ src/parser.py
@@ -5,11 +5,12 @@ import csv
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+
 # This code assumes that the clusters are written down in a sorted fashion
-#np.set_printoptions(threshold=np.nan)
+# np.set_printoptions(threshold=np.nan)
 
 
-def parse(filename, max_window=np.inf):
+def parse(filename, min_window=0, max_window=np.inf):
     # Read all data (in CSV format) from log file
 
     x = np.array([], dtype=int)
@@ -27,16 +28,18 @@ def parse(filename, max_window=np.inf):
             window_counter += 1
             offset += additional_offset + 1
             additional_offset = 0
-        elif line != []:
+        elif line != [] and window_counter >= min_window:
             additional_offset = max(additional_offset, int(line[1]))
-            if len(cluster_indexes) > int(line[0]) + 1:                         # There's already an element of number line[0]+1 in x, so you can insert line[1] right before it
-                x = np.insert(x, cluster_indexes[int(line[0])+1], int(line[1]) + offset)
+            if len(cluster_indexes) > int(line[
+                                              0]) + 1:  # There's already an element of number line[0]+1 in x, so you can insert line[1] right before it
+                x = np.insert(x, cluster_indexes[int(line[0]) + 1], int(line[1]) + offset)
                 Y = np.insert(Y, cluster_indexes[int(line[0]) + 1], float(line[2]))
                 for index, element in enumerate(cluster_indexes):
-                    if index >= int(line[0])+1:
+                    if index >= int(line[0]) + 1:
                         cluster_indexes[index] = element + 1
-            else:                                                              # You can't insert line[1] right before the first element of number line[0]+1, because there's no such element
-                if int(line[0]) == len(cluster_indexes):                        # If this is the first element characterized by the current number, you have to save its address into cluster_indexes
+            else:  # You can't insert line[1] right before the first element of number line[0]+1, because there's no such element
+                if int(line[0]) == len(
+                        cluster_indexes):  # If this is the first element characterized by the current number, you have to save its address into cluster_indexes
                     cluster_indexes = np.append(cluster_indexes, len(x))
                 x = np.append(x, int(line[1]) + offset)
                 Y = np.append(Y, float(line[2]))
@@ -45,6 +48,7 @@ def parse(filename, max_window=np.inf):
     f_in.close()
 
     return [x, Y, cluster_indexes, window_counter]
+
 
 def filter_angles(x, Y, min_angle=0., max_angle=180.):
     x2 = np.array([], dtype=int)
@@ -58,8 +62,8 @@ def filter_angles(x, Y, min_angle=0., max_angle=180.):
 
     return [x, Y]
 
+
 def plot(x, Y, cluster_indexes, clust_min=0, clust_max=np.inf):
-        
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.set_xlabel('Sample')
@@ -70,14 +74,17 @@ def plot(x, Y, cluster_indexes, clust_min=0, clust_max=np.inf):
             continue
         if i == len(cluster_indexes) - 1:
             # Go from here to end of x/Y array
-            ax.plot(x[cluster_indexes[i] : ], Y[cluster_indexes[i] : ], c='C%i' % i)
+            ax.plot(x[cluster_indexes[i]:], Y[cluster_indexes[i]:], c='C%i' % i)
             ax.scatter(x[cluster_indexes[i]:], Y[cluster_indexes[i]:], c='C%i' % i, marker='x')
         else:
             # Go from here to cluster_indexes[i+1]
-            ax.plot(x[cluster_indexes[i] : cluster_indexes[i+1]], Y[cluster_indexes[i] : cluster_indexes[i+1]], c='C%i' % i)
-            ax.scatter(x[cluster_indexes[i]: cluster_indexes[i + 1]], Y[cluster_indexes[i]: cluster_indexes[i + 1]], c='C%i' % i, marker='x')
+            ax.plot(x[cluster_indexes[i]: cluster_indexes[i + 1]], Y[cluster_indexes[i]: cluster_indexes[i + 1]],
+                    c='C%i' % i)
+            ax.scatter(x[cluster_indexes[i]: cluster_indexes[i + 1]], Y[cluster_indexes[i]: cluster_indexes[i + 1]],
+                       c='C%i' % i, marker='x')
     ax.grid(True)
     plt.show()
+
 
 # # Print all local variabels to file
 # f = open("variables.csv", "w")
@@ -88,6 +95,9 @@ def plot(x, Y, cluster_indexes, clust_min=0, clust_max=np.inf):
 # f.close()
 
 if __name__ == "__main__":
-    [x, Y, cluster_indexes, window_counter] = parse('./inputs/log_file.txt', max_window=15)
+
+    [x, Y, cluster_indexes, window_counter] = parse('../inputs/log_file.txt', min_window = 3, max_window = 4)
     [x, Y] = filter_angles(x, Y)
     plot(x, Y, cluster_indexes)
+    print(cluster_indexes)
+
