60881cd41b331bf606ce5c1aeb2a08f861bb1016
Gabriele Oliaro <gabriele_oliaro@college.harvard.edu>
2018-07-11 17:42:10 +0200

    allow the use of both 2 and 3 dimensions by adjusting the parameter D in test_omgp.py

--- src/test_omgp.py
+++ src/test_omgp.py
@@ -19,9 +19,9 @@ def test_omgp():
 
     # Number of time instants per GP, dimensions and GPs
 
-    n = 50
-    D = 2
-    M = 3
+    n = 100
+    D = 3
+    M = 4
 
 
     # Tunable hyperparameters
@@ -45,8 +45,15 @@ def test_omgp():
 
     # Initialize Plot
     fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.scatter(np.squeeze(np.asarray(x_train)), Y_train[:, 0], c='k', marker='x')  # Add scattered points
+    if D == 2:
+        ax = fig.add_subplot(111)
+        ax.scatter(np.squeeze(np.asarray(x_train)), Y_train[:, 0], c='k', marker='x')  # Add scattered points
+    else: # The graph will be in 3D (dataset-> 3 dimensions or more)
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(x_train, Y_train[:, 0], Y_train[:, 1], c='k', marker='x')  # Add scattered points
+        ax.set_zlabel('Z Axis')
+        ax.view_init(elev=20, azim=30) #Works!
+    
     ax.set_title("%d trajectories to be separated" % M)  # Add title
     ax.set_xlabel('X Axis')
     ax.set_ylabel('Y Axis')
@@ -61,22 +68,37 @@ def test_omgp():
 
     [nada, label] = qZ.max(1), qZ.argmax(1)
     fig2 = plt.figure()
-    ax2 = fig2.add_subplot(111)
+    if D == 2:
+        ax2 = fig2.add_subplot(111)
+    else:
+        ax2 = fig2.add_subplot(111, projection='3d')
+        ax2.set_zlabel('Z Axis')
+        ax2.view_init(elev=20, azim=30) #Works!
+
     ax2.set_xlabel('X Axis')
     ax2.set_ylabel('Y Axis')
     plt.style.use('default')
     ax2.set_title("OMGP regression")
+    
     for c in range(M):
         x_train_new = np.array([])
         Y_train0_new = np.array([])
+        if D > 2:
+            Y_train1_new = np.array([])
         for i in range(x_train.shape[0]):
             if (label[i] == c):
                 x_train_new = np.append(x_train_new, x_train[i])
                 Y_train0_new = np.append(Y_train0_new, Y_train[i, 0])
-        ax2.scatter(x_train_new, Y_train0_new, c='C%i'%c, marker='x')  # Add scattered points
-        
+                if D > 2:
+                    Y_train1_new = np.append(Y_train1_new, Y_train[i, 1])
 
-        ax2.plot(np.squeeze(np.asarray(x_test)), mu[:, 0, c], c='C%i'%c)
+        if D == 2:
+            ax2.scatter(x_train_new, Y_train0_new, c='C%i'%c, marker='x')  # Add scattered points
+            ax2.plot(np.squeeze(np.asarray(x_test)), mu[:, 0, c], c='C%i'%c)
+        else:
+            ax2.scatter(x_train_new, Y_train0_new, Y_train1_new, c='C%i'%c, marker='x')  # Add scattered points
+            ax2.plot3D(np.squeeze(np.asarray(x_test)), mu[:, 0, c], mu[:, 1, c], c='C%i'%c)
+
     plt.show()
 
     
